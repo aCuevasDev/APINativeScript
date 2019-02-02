b305306abf22c14871afc8a03554efe93fb13ea9
Alex <alexcuevitas@gmail.com>
2019-02-02 14:02:30 +0100

    minor fixes and searchbar(needs test)

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,24 @@
+import { SearchBar } from 'tns-core-modules/ui/search-bar';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ns-searchBar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.css'],
+    moduleId: module.id,
+})
+export class SearchBarComponent {
+    
+    public searchPhrase: string;
+
+    public onSubmit(args) {
+        let searchBar = <SearchBar>args.object;
+        alert("You are searching for " + searchBar.text);
+    }
+
+    public onTextChanged(args) {
+        let searchBar = <SearchBar>args.object;
+        console.log("SearchBar text changed! New value: " + searchBar.text);
+    }
+
+}
